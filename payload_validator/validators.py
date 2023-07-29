f331f8283cc30857c731653a181e2057e38319ea
cwadven <cwadven@naver.com>
2023-07-29 22:01:41 +0900

    TASK-11 Feat: Add comparisons
    - Add comparisons

--- payload_validator/validators.py
+++ payload_validator/validators.py
@@ -1,19 +1,19 @@
-import re
-import traceback
 from abc import (
     ABC,
     abstractmethod,
 )
-from itertools import chain
 from types import FunctionType
 from typing import (
     Callable,
+    Dict,
     Iterable,
+    List,
     Optional,
     TypeVar,
     Union,
 )
 
+from payload_validator.comparisons import PayloadComparison
 from payload_validator.exceptions import (
     InvalidValueError,
     MismatchedErrorKeysException,
@@ -41,20 +41,29 @@ class PayloadValidator(object):
     DEFAULT_MANDATORY_ERROR_MESSAGE = "mandatory data missing"
 
     def __init__(self, payload: dict, error_context: ValidatorErrorContext = None) -> None:
-        self.payload = payload
-        self._skip_validate_keys = set()
-        self._validate_called = False
-        self._error_context = (
+        self.payload: dict = payload
+        self._cache_payload_comparisons: dict = {}
+
+        self.skip_validate_keys: set = set()
+
+        self._error_context: ValidatorErrorContext = (
             error_context
             if error_context is not None
             else NormalValidatorErrorContext()
         )
 
-        self.mandatory_keys = getattr(self._get_meta_attribute(), "mandatory_keys", {})
-        self.type_of_keys = getattr(self._get_meta_attribute(), "type_of_keys", {})
+        self.mandatory_keys: Dict[str, str] = getattr(self._get_meta_attribute(), "mandatory_keys", {})
+        self.type_of_keys: Dict[str, List] = getattr(self._get_meta_attribute(), "type_of_keys", {})
+
+        self._validate_called: bool = False
+
+    def get_payload(self, key: str) -> PayloadComparison:
+        if key not in self._cache_payload_comparisons:
+            self._cache_payload_comparisons[key] = PayloadComparison(self, key)
+        return self._cache_payload_comparisons[key]
 
     def add_error_context(self, key: str, errors: Union[str, Iterable]) -> None:
-        if key not in self._skip_validate_keys:
+        if key not in self.skip_validate_keys:
             if isinstance(errors, str):
                 self._error_context.add_error(key, errors)
             else:
@@ -66,7 +75,7 @@ class PayloadValidator(object):
         add only main error so other errors cannot add
         """
         self.add_error_context(key, errors)
-        self._skip_validate_keys.add(key)
+        self.skip_validate_keys.add(key)
 
     def _get_meta_attribute(self) -> Optional[T]:
         return getattr(self, "Meta", None)
@@ -110,15 +119,6 @@ class PayloadValidator(object):
             else:
                 self.add_error_context(key, value)
 
-    def _handle_skip_validation_key_payload_exception(self, e: Exception) -> None:
-        tb = traceback.extract_tb(e.__traceback__)
-        pattern = r"self\.payload(?:\.get\('([^']*)'\)|\['([^']*)'\]|(?:\[\"|\'|)([^\"\']*)(?:\"|\'|)\])"
-        for key in [match for match in chain(*re.findall(pattern, tb[-1].line)) if match]:
-            if key in self._skip_validate_keys:
-                pass
-            else:
-                raise e
-
     def _common_validate(self) -> None:
         try:
             self.common_validate()
@@ -126,8 +126,6 @@ class PayloadValidator(object):
             raise e
         except InvalidValueError as e:
             self._handle_invalid_value_error_exception(e)
-        except Exception as e:
-            self._handle_skip_validation_key_payload_exception(e)
 
     def common_validate(self) -> None:
         """
@@ -144,8 +142,6 @@ class PayloadValidator(object):
                     raise e
                 except InvalidValueError as e:
                     self._handle_invalid_value_error_exception(e)
-                except Exception as e:
-                    self._handle_skip_validation_key_payload_exception(e)
 
     def validate(self) -> None:
         if not self._validate_called:
