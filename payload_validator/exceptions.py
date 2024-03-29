fe2bdcefd34e10911fe1d400f74012ad13e608ca
cwadven <cwadven@naver.com>
2024-03-29 23:49:06 +0900

    Feat: Use ignore_existing_error_keys for already error then skip
    - Use ignore_existing_error_keys for already error then skip

--- payload_validator/exceptions.py
+++ payload_validator/exceptions.py
@@ -19,13 +19,13 @@ class MismatchedErrorKeysException(Exception):
 
 
 class InvalidValueError(Exception):
-    def __init__(self, error_value_by_key: Dict[str, Union[str, Iterable]], add_skip_validation_keys: list = None):
-        if add_skip_validation_keys is None:
-            add_skip_validation_keys = []
-        result = [key for key in add_skip_validation_keys if key not in error_value_by_key.keys()]
+    def __init__(self, error_value_by_key: Dict[str, Union[str, Iterable]], ignore_existing_error_keys: set = None):
+        if ignore_existing_error_keys is None:
+            ignore_existing_error_keys = set()
+        result = [key for key in ignore_existing_error_keys if key not in error_value_by_key.keys()]
         if result:
             raise MismatchedErrorKeysException(
-                "In add_skip_validation_keys {} not in error_value_by_key".format(', '.join(result))
+                "In ignore_existing_error_keys {} not in error_value_by_key".format(', '.join(result))
             )
         self.error_value_by_key = error_value_by_key
-        self.add_skip_validation_keys = add_skip_validation_keys
+        self.ignore_existing_error_keys = ignore_existing_error_keys
